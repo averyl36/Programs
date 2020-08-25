ed26c618a4546d3486be5ef23818d0c43dee0350
unknown <averyl36@yahoo.com>
2020-08-24 21:24:21 -0600

    Add existing file

---
+++ HelloWorld/src/edu/nmsu/cs/helloworld.java
@@ -0,0 +1,14 @@
+// Avery Lee
+// Z Toups
+// CS 371
+// August 24, 2020
+
+public class helloworld {
+	
+	public static void main (String args[]) {
+		
+		System.out.println("Hello World");
+		
+	} // end main
+	
+} // end class
